518b347f6d19f26c7dad3a32ec60ac2235f77fc0
ghbntyuizs <63224777+ghbntyuizs@users.noreply.github.com>
2024-07-09 01:31:59 +0700

    done

---
+++ MusicApp/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+
+namespace MusicApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if( e.ChangedButton == MouseButton.Left )
+            {
+                this.DragMove();
+            }
+        }
+
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+    }
+}
